aa78cd5222fce1217557379fc91a0fc704f27726
andrew-savich <savichandrew@Gmail.com>
2021-05-11 21:44:02 +0300

    Adding a new product

--- product-manager/src/main/java/com/andrewsavich/productmanager/controller/AppController.java
+++ product-manager/src/main/java/com/andrewsavich/productmanager/controller/AppController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.andrewsavich.productmanager.model.Product;
 import com.andrewsavich.productmanager.service.ProductService;
@@ -30,5 +32,12 @@ public class AppController {
 		model.addAttribute("product", product);
 
 		return "new_product";
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveNewProduct(@ModelAttribute("product") Product product) {
+		service.savevProduct(product);
+		
+		return "redirect:/";
 	}
 }
